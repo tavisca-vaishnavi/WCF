78f45e5861653037df76a70ae9efae639b3893d5
Vaishnavi <vgalgali@tavisca.com>
2014-08-16 17:26:02 +0530

    WCF working service Also Added client implementation

---
+++ assg/assg/Service1.svc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace assg
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IAddEmpRecords, IRetrieveDetails
+    {
+        static public List<Employee> EmpList = new List<Employee>();
+
+        public string AddEmployee(string id, string name)
+        {
+            Employee emp = new Employee();
+            emp.id = id;
+            emp.Name = name;
+            EmpList.Add(emp);
+            return ("Added Record");
+        }
+
+        public Employee AddRemarks(string id, string remarks)
+        {
+           
+            foreach (Employee e in EmpList)
+            {
+                if (e.id.Equals(id))
+                {
+                    e.Remarks = remarks;
+                    return e;
+                }
+            }
+            throw new FaultException(new FaultReason("Employee Record Not Found For Inserting Remarks"), new FaultCode("102"));
+        }
+
+        public Employee Search(string id)
+        {
+            Employee emp = new Employee();
+            foreach (Employee e in EmpList)
+            {
+                if (e.id.Equals(id))
+                    return e;
+            }
+
+            throw new FaultException(new FaultReason("Employee Record Not Found"), new FaultCode("101"));
+        }
+
+
+        public int delete(string id)
+        {
+            foreach (Employee e in EmpList)
+            {
+                if (e.id.Equals(id))
+                {
+                    EmpList.Remove(e);
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        IEnumerable<Employee> IRetrieveDetails.GetAllDetails()
+        {
+            return EmpList;
+        }
+    }
+}
---
+++ assg/client/Program.cs
@@ -0,0 +1,148 @@
+﻿using client.myservice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddEmpRecordsClient client = new AddEmpRecordsClient();
+            RetrieveDetailsClient client2 = new RetrieveDetailsClient();
+            string choice;
+            int ch;
+            do
+            {
+                Console.WriteLine("************ MENU *************");
+                Console.WriteLine("1. Add Records");
+                Console.WriteLine("2. Search Records");
+                Console.WriteLine("3. Add Remarks to Records");
+                Console.WriteLine("4. Get All Records");
+                Console.WriteLine("5. Delete");
+                Console.WriteLine("6. Exit");
+                Console.WriteLine("Enter your Choice:");
+                choice = Console.ReadLine();
+                bool output = Int32.TryParse(choice, out ch);
+
+                switch (ch)
+                {
+                    case 1:
+                        //Add Records
+                        Console.WriteLine("Enter Id");
+                        string id = Console.ReadLine();
+                        Console.WriteLine("Enter Name");
+                        string name = Console.ReadLine();
+                        string result = client.AddEmployee(id, name);
+                        Console.WriteLine(result);
+                        break;
+
+                    case 2:
+                        //search records
+                        Console.WriteLine("Enter Id");
+                        string idForSearch = Console.ReadLine();
+                        Employee SearchedEmp = new Employee();
+                        SearchedEmp = client2.Search(idForSearch);
+                        if (SearchedEmp != null)
+                        {
+                            Console.WriteLine("Required Emp ID " + SearchedEmp.id);
+                            Console.WriteLine("Required Emp Name " + SearchedEmp.Name);
+                            Console.WriteLine("Required Emp Remarks " + SearchedEmp.Remarks);
+                        }
+                        else
+                            Console.WriteLine("No Records Found");
+                        break;
+
+                    case 3:
+                        //Add Remarks To Records
+                        Console.WriteLine("Enter Employee Id");
+                        string idForRemarks = Console.ReadLine();
+                        Console.WriteLine("Enter Remarks");
+                        string remarks = Console.ReadLine();
+                        Employee e_obj = new Employee();
+                        e_obj = client.AddRemarks(idForRemarks, remarks);
+                        if (e_obj != null)
+                        {
+                            Console.WriteLine("Emp ID" + e_obj.id);
+                            Console.WriteLine("Emp ID" + e_obj.Name);
+                            Console.WriteLine("Emp ID" + e_obj.Remarks);
+                        }
+                        break;
+
+                    case 4:
+                        //Get All Records
+                        Console.WriteLine("***** All Records *****");
+                        List<Employee> AllEmpDetails = new List<Employee>();
+                        AllEmpDetails = client2.GetAllDetails();
+                        foreach (Employee e1 in AllEmpDetails)
+                        {
+                            Console.WriteLine("Emp ID" + e1.id);
+                            Console.WriteLine("Emp ID" + e1.Name);
+                            Console.WriteLine("Emp ID" + e1.Remarks);
+                        }
+                        break;
+
+                    case 5:
+                        //Delete Record
+                        Console.WriteLine("Enter Id to delete Record");
+                        string idToDelete = Console.ReadLine();
+                        int del = client.delete(idToDelete);
+                        if (del == 1)
+                            Console.WriteLine("Successfully Deleted");
+                        else
+                            Console.WriteLine("Failed to Delete Record");
+                        break;
+                }
+            } while (ch != 6);
+
+
+
+
+
+
+
+
+            //Added
+            //string result=client.AddEmployee("101", "drux");
+            //Console.WriteLine(result);
+            //search
+            Employee e = new Employee();
+            //e = client2.Search("101");
+            //if (e != null)
+            //{
+            //    Console.WriteLine("Emp ID" + e.id);
+            //    Console.WriteLine("Emp ID" + e.Name);
+            //    // Console.WriteLine("Emp ID" + e.Remarks);
+            //}
+
+            //all details
+            //List<Employee> AllEmpDetails = new List<Employee>();
+            //AllEmpDetails = client2.GetAllDetails();
+            //foreach (Employee e1 in AllEmpDetails)
+            //{
+            //    Console.WriteLine("Emp ID" + e1.id);
+            //    Console.WriteLine("Emp ID" + e1.Name);
+            //    Console.WriteLine("Emp ID" + e1.Remarks);
+            //}
+
+            ////Add Remarks
+            //Employee e_obj = new Employee();
+            //e_obj = client.AddRemarks("101", "very poor");
+            //if (e != null)
+            //{
+            //    Console.WriteLine("Emp ID" + e_obj.id);
+            //    Console.WriteLine("Emp ID" + e_obj.Name);
+            //    Console.WriteLine("Emp ID" + e_obj.Remarks);
+            //}
+
+            //delete
+            //int del = client.delete("101");
+            //Console.WriteLine("deleted");
+
+           
+        }
+    }
+}
